f5cf5b4bcff0791958df81bb6f8a7726b225fe87
Frank Liu <fliu@universalbiosensors.com>
2016-09-23 10:34:44 +1000

    initial

---
+++ TaskQueueAndCancellationPractise/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TaskQueueAndCancellationPractise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Random _delayRandomizer;
+        private readonly CancellationTokenSource _cancellationSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _delayRandomizer = new Random(DateTime.Now.Millisecond);
+            _cancellationSource = new CancellationTokenSource();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as MainWindowViewModel;
+            var id = "Button1Task";
+
+            QueueButtonTask(vm, id);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as MainWindowViewModel;
+            var id = "Button2Task";
+
+            QueueButtonTask(vm, id);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as MainWindowViewModel;
+            var id = "Button3Task";
+
+            QueueButtonTask(vm, id);
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as MainWindowViewModel;
+            var id = "Button4Task";
+
+            QueueButtonTask(vm, id);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as MainWindowViewModel;
+            var id = "Button5Task";
+
+            QueueButtonTask(vm, id);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as MainWindowViewModel;
+            var id = "Button6Task";
+
+            QueueButtonTask(vm, id);
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as MainWindowViewModel;
+            var id = "Button7Task";
+
+            QueueButtonTask(vm, id);
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            var vm = this.DataContext as MainWindowViewModel;
+            var id = "Button8Task";
+
+            QueueButtonTask(vm, id);
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            _cancellationSource.Cancel();
+        }
+
+        private void QueueButtonTask(MainWindowViewModel vm, string id)
+        {
+            if (vm.TasksRunning.Any(x => x.Id == id))
+                return;
+
+            //put the tasks that is click on the height priority
+            if (vm.TasksAwaitRunning.Any(x => x.Id == id))
+            {
+                vm.UpdateAwaitTaskPriority(id);
+                return;
+            }
+
+            var delay = _delayRandomizer.Next(5, 15);
+            var appTask = new AppTask
+            {
+                Id = id,
+                Delay = delay,
+                Task = new Task(() => LongRunningClass.LongRunningMethod(delay), _cancellationSource.Token, TaskCreationOptions.None)
+            };
+
+            appTask.Task.ContinueWith(t => vm.OnTaskComplete(id), 
+                CancellationToken.None,
+                TaskContinuationOptions.OnlyOnRanToCompletion,
+                TaskScheduler.FromCurrentSynchronizationContext());
+
+            appTask.Task.ContinueWith(t => vm.OnTaskCanceled(id),
+                CancellationToken.None,
+                TaskContinuationOptions.OnlyOnCanceled, 
+                TaskScheduler.FromCurrentSynchronizationContext());
+
+            vm.QueueNewTask(appTask);
+        }
+    }
+}
---
+++ TaskQueueAndCancellationPractise/MainWindowViewModel.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskQueueAndCancellationPractise
+{
+    public class MainWindowViewModel
+    {
+        private readonly ObservableCollection<AppTask> _tasksAwaitRunning;
+        private readonly ObservableCollection<AppTask> _tasksRunning;
+        private readonly ObservableCollection<AppTask> _tasksCompleted;
+        private readonly ObservableCollection<AppTask> _tasksCanceled;
+        private readonly ObservableCollection<AppTask> _allTasks;
+        private Object taskLock = new Object();
+
+        public int CoreNumber { get { return Environment.ProcessorCount; }}
+
+        public MainWindowViewModel()
+        {
+            _tasksAwaitRunning = new ObservableCollection<AppTask>();
+            _tasksRunning = new ObservableCollection<AppTask>();
+            _tasksCompleted = new ObservableCollection<AppTask>();
+            _tasksCanceled = new ObservableCollection<AppTask>();
+            _allTasks = new ObservableCollection<AppTask>();
+        }
+
+        public ObservableCollection<AppTask> TasksAwaitRunning { get { return _tasksAwaitRunning; } }
+        public ObservableCollection<AppTask> TasksRunning { get { return _tasksRunning; } }
+        public ObservableCollection<AppTask> TasksAll { get { return _allTasks; } }
+        public ObservableCollection<AppTask> TasksCompleted { get { return _tasksCompleted; } }
+        public ObservableCollection<AppTask> TasksCanceled { get { return _tasksCanceled; } }
+
+        public void OnTaskComplete(string id)
+        {
+            lock (taskLock)
+            {
+                var task = _tasksRunning.First(x => x.Id == id);
+
+                _tasksRunning.Remove(task);
+                _tasksCompleted.Add(task);
+
+                if (_tasksAwaitRunning.Any())
+                {
+                    var next = _tasksAwaitRunning.OrderBy(x => x.Priority).First();
+                    _tasksAwaitRunning.Remove(next);
+                    next.Task.Start(TaskScheduler.Default);
+                    _tasksRunning.Add(next);
+                }
+            }
+        }
+
+        public void UpdateAwaitTaskPriority(string id)
+        {
+            lock (taskLock)
+            {
+                var task = _tasksAwaitRunning.First(x => x.Id == id);
+
+                task.Priority = 0;
+                var otherPriority = 1;
+                foreach (var otherTask in _tasksAwaitRunning.Where(x => x.Id != id))
+                {
+                    otherTask.Priority = otherPriority++;
+                }
+            }
+        }
+
+        public void OnTaskCanceled(string id)
+        {
+            lock (taskLock)
+            {
+                var task = _tasksRunning.FirstOrDefault(x => x.Id == id);
+
+                if (task != null)
+                {
+                    _tasksRunning.Remove(task);
+                }
+                else
+                {
+                    task = _tasksAwaitRunning.FirstOrDefault(x => x.Id == id);
+                    _tasksAwaitRunning.Remove(task);
+                }
+
+                _tasksCanceled.Add(task);
+
+                //if (_tasksAwaitRunning.Any())
+                //{
+                //    var next = _tasksAwaitRunning.First();
+                //    _tasksAwaitRunning.Remove(next);
+                //    next.Task.Start(TaskScheduler.Default);
+                //    _tasksRunning.Add(next);
+                //}
+            }
+        }
+
+        public void QueueNewTask(AppTask appTask)
+        {
+            lock (taskLock)
+            {
+                _allTasks.Add(appTask);
+
+                if (_tasksRunning.Count < Environment.ProcessorCount)
+                {
+                    appTask.Task.Start(TaskScheduler.Default);
+                    _tasksRunning.Add(appTask);
+                }
+                else
+                {
+                    appTask.Priority = _tasksAwaitRunning.Count;
+                    _tasksAwaitRunning.Add(appTask);
+                }
+            }
+        }
+
+
+    }
+
+    public class AppTask
+    {
+        public AppTask()
+        {
+            Priority = 0;
+        }
+
+        public Task Task { get; set; }
+        public String Id { get; set; }
+        public int Delay { get; set; }
+        public int Priority { get; set; }
+
+        public override string ToString()
+        {
+            return Id + "   Delay: " + Delay;
+        }
+    }
+
+    public static class LongRunningClass
+    {
+        public static void LongRunningMethod(int delay)
+        {
+            Thread.Sleep(delay * 1000);
+        }
+    }
+}
